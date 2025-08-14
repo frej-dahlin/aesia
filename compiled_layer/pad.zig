8dcca50f8d3593c5afd827942eb16a103e6860fe
Emil Engstrom <emil-engstrom@hotmail.com>
2025-08-14 19:48:46 +0200

    Fixed bitset import

--- compiled_layer/pad.zig
+++ compiled_layer/pad.zig
@@ -1,6 +1,6 @@
 const std = @import("std");
 
-const StaticBitSet = @import("bitset.zig").StaticBitSet;
+const StaticBitSet = @import("../src/bitset.zig").StaticBitSet;
 pub const logic = @import("logic.zig");
 
 pub const GateRepresentation = logic.GateRepresentation;
